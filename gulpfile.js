6c6419e22e5a9027386ddd9ee201e92de0414e06
eguneys <eguneys@hotmail.com>
2016-05-31 12:39:45 +0300

    css layout changed for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var source = require('vinyl-source-stream');
 var gulp = require('gulp');
 var tap = require('gulp-tap');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
 var jshint = require('gulp-jshint');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 
-var sass = require('gulp-sass');
-
 var assetFiles = './assets/**/*';
-var sassFiles = ['./sass/*.scss'];
+var cssFiles = ['./assets/*.css'];
 var sources = ['./src/main.js'];
 var destination = './build';
 var onError = function(error) {
@@ -26,7 +26,7 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('prod', ['sass'], function() {
+gulp.task('prod', ['prod-css'], function() {
   return browserify('./src/main.js', {
     standalone: standalone
   }).transform('babelify',
@@ -39,40 +39,26 @@ gulp.task('prod', ['sass'], function() {
     .pipe(gulp.dest(destination));
 });
 
-gulp.task('prod-watch', ['sass'], function() {
-  var opts = watchify.args;
-  opts.debug = true;
-  opts.standalone = standalone;
-  var bundleStream = watchify(browserify(sources, opts))
-    .transform('babelify',
-               { presets: ["es2015"],
-                 plugins: ['add-module-exports'] })
-    .on('update', rebundle)
-    .on('log', gutil.log);
-
-  function rebundle() {
-    return bundleStream.bundle()
-      .on('error', onError)
-      .pipe(source('okeyground.min.js'))
-      .pipe(gulp.dest(destination));
-  }
-
-  return rebundle();
-});
-
 gulp.task('assets', function() {
   var path = require('path');
   gulp.src(assetFiles)
     .pipe(gulp.dest(path.join(destination)));
 });
 
-gulp.task('sass', function() {
-  return gulp.src(sassFiles)
-    .pipe(sass().on('error', sass.logError))
+gulp.task('css', function() {
+  return gulp.src(cssFiles)
+    .pipe(concat('main.css'))
     .pipe(gulp.dest(destination));
 });
 
-gulp.task('dev', ['assets'], function() {
+gulp.task('prod-css', function() {
+  return gulp.src(cssFiles)
+    .pipe(csso())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('dev', ['assets', 'css'], function() {
   var opts = watchify.args;
   opts.debug = true;
   opts.standalone = standalone;
@@ -90,7 +76,7 @@ gulp.task('dev', ['assets'], function() {
       .pipe(gulp.dest(destination));
   }
 
-  gulp.watch(sassFiles, ['sass']);
+  gulp.watch(cssFiles, ['css']);
 
   return rebundle();
 });
